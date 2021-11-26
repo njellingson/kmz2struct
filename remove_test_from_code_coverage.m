0c3a17001112e337f8ab280220fea6033e94884f
Nathan Ellingson <ellingsonnj@gmail.com>
2021-11-25 22:34:39 -0500

    Fix code coverage

--- remove_test_from_code_coverage.m
+++ remove_test_from_code_coverage.m
@@ -14,9 +14,16 @@ doc = xmlread(code_coverage_results_file);
 % Get all the functions with code coverage results
 files = doc.getElementsByTagName('class');
 
+% Save files in a cell array so it's easier to remove ones not needed
+n_files = files.getLength;
+files_cell = cell([n_files 1]);
+for i = 1:n_files
+    files_cell{i} = files.item(i-1);
+end
+
 % Loop through the files and remove the ones that are not useful
-for i = 1:files.getLength
-    current_node = files.item(i-1);
+for i = 1:n_files
+    current_node = files_cell{i};
     current_file = char(current_node.getAttribute('name'));
     remove = ismember(current_file,files_to_exclude);
     if remove
